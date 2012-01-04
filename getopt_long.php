c937837264265ff4544da653eb87169ea7356f1b
Paul Wayper <paulway@mabula.net>
2012-01-04 01:58:18 +0000

    Moved processing of each argument to separate function (not a perfect way to do it).  This means we can process multiple options per command-line argument (i.e. option concatenation).

--- getopt_long.php
+++ getopt_long.php
@@ -12,6 +12,13 @@
 
 # Version 1.0 - initial release.
 # Version 1.1 - added array output for mandatory arguments
+# Version 1.2 - dash length now has significance - single-dash options are
+#   always assumed to be groups of single-letter options (e.g. tar -cvfz) and
+#   double-dash always assumed to be full words.  A flag to turn this off is
+#   provided in $GODashLengthSignificant (defaults to true).
+
+$GODashLengthSignificant = TRUE;
+$GODebug = FALSE;
 
 function GOCheckType($arg, $type) {
     if ($type == 's') {
@@ -25,10 +32,99 @@ function GOCheckType($arg, $type) {
     }
 }
 
+function GOProcessArgument($opt, $args, $i, $numargs, $arg_lookup) {
+	global $GODebug;
+    if ($GODebug) {
+		print("  And it's an option we recognise\n");
+    }
+	$opt_info = $arg_lookup[$opt];
+	# Does it have any arguments?
+	if (array_key_exists('opt',$opt_info)) {
+		$opt = $opt_info['opt'];
+		if ($opt == '=') { # mandatory argument
+			$i++;
+			# Is there still command line left?
+			if ($i < $numargs) {
+				# Yes: set the variable from the argument list
+				# We're even allowed to take things that look
+				# like options here!
+				# Check its type here.
+				if (array_key_exists('dest',$opt_info) and $opt_info['dest'] == '@') {
+					# Explicitly require array
+					# variable may not be array - convert if so
+					if (is_array($opt_info['var'])) {
+			            if ($GODebug) {
+							print("  it takes an array parameter and is one: pushing $args[$i] to it\n");
+		                }
+						$opt_info['var'][] = $args[$i]; # Push to array
+					} else {
+			            if ($GODebug) {
+							print("  it takes an array parameter and isn't one: setting its variable to an array of ($args[$i])\n");
+		                }
+						# Convert to two-value array.
+						$opt_info['var'] = array($args[$i]);
+					}
+				} else if (is_array($opt_info['var'])) {
+	                if ($GODebug) {
+						print("  it takes a parameter and we've been given an array: pushing $args[$i] onto it\n");
+                    }
+					# @ not specified but array reference given
+					$opt_info['var'][] = $args[$i]; # Push to array
+				} else {
+	                if ($GODebug) {
+						print("  it takes a parameter: setting its variable to $args[$i]\n");
+                    }
+					$opt_info['var'] = $args[$i];
+				}
+			} else {
+				# No: fail.
+				die("GetOptions: argument $arg missing its parameter\n");
+			}
+		} else if ($opt == ':') { # optional argument
+		    # Is there still another option left?
+		    if ($i+1 == $numargs) {
+		    	# No - no argument supplied, set the variable to 1
+                if ($GODebug) {
+			    	print("  optional argument, none available: value 1\n");
+                }
+		    	$opt_info['var'] = 1;
+		    } else {
+		    	# Does the next option look like a flag?
+		    	if (substr($args[$i+1],0,1) == '-') {
+		    		# Yes - no argument supplied, set variable to 1
+                    if ($GODebug) {
+				    	print("  optional argument, next one starts with '-': value 1\n");
+                    }
+			    	$opt_info['var'] = 1;
+		    	} else {
+		    		# No - it must be an argument, consume it
+		    		$i++;
+                    if ($GODebug) {
+			    		print("  optional argument, one supplied, setting variable to $args[$i]\n");
+                    }
+					# Check its type here.
+					$opt_info['var'] = $args[$i];
+		    	}
+		    }
+		} else if ($opt == '+') { # incrementing argument
+			$opt_info['var'] ++;
+            if ($GODebug) {
+				print("  it's an incrementing argument, setting its variable to $opt_info[var]\n");
+            }
+		}
+	} else {
+		# No args, just a boolean, set it:
+        if ($GODebug) {
+			print("  it's a boolean: setting its variable from $opt_info[var] to 1\n");
+        }
+		$opt_info['var'] = 1;
+	}
+}
+
 function GetOptions($argDescriptions) {
-	$debug = FALSE;
+	global $GODebug;
+	global $GODashLengthSignificant;
 	# Preprocess argument descriptions to look up names and info
-	$arg_lookup = array();
 	# foreach key => val doesn't respect references - use keys only
 	foreach (array_keys($argDescriptions) as $argdesc) {
 		# Pull apart the arguments into a list of synonyms and then the
@@ -52,7 +148,7 @@ function GetOptions($argDescriptions) {
 				if (strlen($optstr) > 2) {
 					$opt_info['dest'] = substr($optstr,2,1);
 				}
-				if ($debug) {
+				if ($GODebug) {
 					print("Opt info opt = $opt_info[opt], type = $opt_info[type]\n");
 				}
 			}
@@ -62,7 +158,7 @@ function GetOptions($argDescriptions) {
 		        print("Warning: key $synonyms started or ended with |.\n");
 		        continue;
 		    }
-			if ($debug) {
+			if ($GODebug) {
 			    print("Putting synonym $synonym of $synonyms in arg_lookup\n");
 		    }
 			$arg_lookup[$synonym] = $opt_info;
@@ -75,7 +171,7 @@ function GetOptions($argDescriptions) {
 	$i = 0; $numargs = count($args);
 	while ($i < $numargs) {
 		$arg = $args[$i];
-		if ($debug) {
+		if ($GODebug) {
     		print("Processing argument $i: $arg\n");
 	    }
 		if ($arg == '--') {
@@ -90,95 +186,12 @@ function GetOptions($argDescriptions) {
 			} else {
 				$opt = substr($arg,1);
 			}
-		    if ($debug) {
+		    if ($GODebug) {
     			print(" Looks like option $opt.\n");
 	        }
+	        # We're sharing a lot of state between the two - is this fragile?
 			if (array_key_exists($opt,$arg_lookup)) {
-		        if ($debug) {
-    				print("  And it's an option we recognise\n");
-	            }
-				$opt_info = $arg_lookup[$opt];
-				# Does it have any arguments?
-				if (array_key_exists('opt',$opt_info)) {
-					$opt = $opt_info['opt'];
-					if ($opt == '=') { # mandatory argument
-						$i++;
-						# Is there still command line left?
-						if ($i < $numargs) {
-							# Yes: set the variable from the argument list
-							# We're even allowed to take things that look
-							# like options here!
-							# Check its type here.
-							if (array_key_exists('dest',$opt_info) and $opt_info['dest'] == '@') {
-								# Explicitly require array
-								# variable may not be array - convert if so
-								if (is_array($opt_info['var'])) {
-						            if ($debug) {
-										print("  it takes an array parameter and is one: pushing $args[$i] to it\n");
-					                }
-									$opt_info['var'][] = $args[$i]; # Push to array
-								} else {
-						            if ($debug) {
-										print("  it takes an array parameter and isn't one: setting its variable to an array of ($args[$i])\n");
-					                }
-									# Convert to two-value array.
-									$opt_info['var'] = array($args[$i]);
-								}
-							} else if (is_array($opt_info['var'])) {
-				                if ($debug) {
-									print("  it takes a parameter and we've been given an array: pushing $args[$i] onto it\n");
-			                    }
-								# @ not specified but array reference given
-								$opt_info['var'][] = $args[$i]; # Push to array
-							} else {
-				                if ($debug) {
-									print("  it takes a parameter: setting its variable to $args[$i]\n");
-			                    }
-								$opt_info['var'] = $args[$i];
-							}
-						} else {
-							# No: fail.
-							die("GetOptions: argument $arg missing its parameter\n");
-						}
-					} else if ($opt == ':') { # optional argument
-					    # Is there still another option left?
-					    if ($i+1 == $numargs) {
-					    	# No - no argument supplied, set the variable to 1
-		                    if ($debug) {
-    					    	print("  optional argument, none available: value 1\n");
-	                        }
-					    	$opt_info['var'] = 1;
-					    } else {
-					    	# Does the next option look like a flag?
-					    	if (substr($args[$i+1],0,1) == '-') {
-					    		# Yes - no argument supplied, set variable to 1
-		                        if ($debug) {
-    						    	print("  optional argument, next one starts with '-': value 1\n");
-	                            }
-						    	$opt_info['var'] = 1;
-					    	} else {
-					    		# No - it must be an argument, consume it
-					    		$i++;
-		                        if ($debug) {
-    					    		print("  optional argument, one supplied, setting variable to $args[$i]\n");
-	                            }
-    							# Check its type here.
-								$opt_info['var'] = $args[$i];
-					    	}
-					    }
-					} else if ($opt == '+') { # incrementing argument
-						$opt_info['var'] ++;
-	                    if ($debug) {
-    						print("  it's an incrementing argument, setting its variable to $opt_info[var]\n");
-                        }
-					}
-				} else {
-					# No args, just a boolean, set it:
-		            if ($debug) {
-    					print("  it's a boolean: setting its variable from $opt_info[var] to 1\n");
-	                }
-					$opt_info['var'] = 1;
-				}
+		        GOProcessArgument($opt, &$args, &$i, &$numargs, &$arg_lookup);
 			} else {
 				# Not a recognised argument argument: leave it unprocessed.
 				$unprocessed_args[] = $arg;
