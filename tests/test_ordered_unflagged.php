79d47950ccb86c026aab7f9b4161273da0267506
Paul Wayper <paulway@mabula.net>
2012-07-16 11:43:45 +0000

    Adding new (failing) tests.

--- tests/test_ordered_unflagged.php
+++ tests/test_ordered_unflagged.php
@@ -5,8 +5,8 @@ require_once "../Console/GetoptLong.php";
 $first = '';
 $second = '';
 $args = Console_GetoptLong::getOptions(array(
-	'first|f|_0=s'  => &$first,
-	'_1=s'          => &$second,
+	'first|f|_1=s'  => &$first,
+	'second|s|_2=s' => &$second,
 ));
 
 echo "$first,$second,(", implode(',',$args), ")\n";
