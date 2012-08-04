bd6e2614eafb366bade5d0b81dc2977e55aa401e
Paul Wayper <paulway@mabula.net>
2012-08-04 00:41:03 +0000

    Test one mandatory and one optional ordered unflagged argument each.

--- tests/test_ordered_unflagged.php
+++ tests/test_ordered_unflagged.php
@@ -6,7 +6,7 @@ $first = '';
 $second = '';
 $args = Console_GetoptLong::getOptions(array(
 	'first|f|_1=s'  => &$first,
-	'second|s|_2=s' => &$second,
+	'second|s|_2:s' => &$second,
 ));
 
 echo "$first,$second,(", implode(',',$args), ")\n";
